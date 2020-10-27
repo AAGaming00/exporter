cff70a4926082fb00c3a03ade5af2c265a525914
AA <aa@bigdumb.gq>
2020-10-27 09:37:35 -0400

    update a bunch of stuff

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function toId (id) {
   return `chat-messages-${id}`;
 }
 function idIndex (id, array) {
-  const filter = array.filter(x => x.id === id);
+  const filter = array.filter((x) => x.id === id);
   return array.indexOf(filter[0]);
 }
 module.exports = class Exporter extends Plugin {
@@ -28,7 +28,9 @@ module.exports = class Exporter extends Plugin {
 
   async export (start, end) {
     console.log(start, end);
-    const elem = start ? byId(toId(start)) : document.querySelectorAll('[id*="chat-messages-"]')[0];
+    const elem = start
+      ? byId(toId(start))
+      : document.querySelectorAll('[id*="chat-messages-"]')[0];
     if (!start) {
       start = elem.id.replace('chat-messages-', '');
     }
@@ -38,52 +40,113 @@ module.exports = class Exporter extends Plugin {
     if (startIndex === -1) {
       return;
     }
-    const toExport = children.slice(startIndex, endIndex !== -1 ? endIndex : undefined).filter(x => x.className !== 'navigationDescription-3hiGKr' && x.className !== 'wrapper-3vR61M');
+    const toExport = children
+      .slice(startIndex, endIndex !== -1 ? endIndex : undefined)
+      .filter(
+        (x) =>
+          x.className !== 'navigationDescription-3hiGKr' &&
+          x.className !== 'wrapper-3vR61M'
+      );
     if (!end) {
-      end = toExport[toExport.length - 2].id.replace('chat-messages-', '');
+      end = toExport[toExport.length - 2].id.replace(
+        'chat-messages-',
+        ''
+      );
     }
     console.log(startIndex, endIndex, toExport);
-    let messages = toExport.map(x => x.outerHTML).join('\n');
-    powercord.api.notices.sendToast(`exporter-${(Math.random().toString(36) + Date.now()).substring(2, 7)}`, {
-      header: 'Archiving',
-      content: 'Archiving messages...\nMake sure to include the assets folder relative to the file!',
-      icon: 'history',
-      timeout: 3e3
-    });
+    let messages = toExport.map((x) => x.outerHTML).join('\n');
+    powercord.api.notices.sendToast(
+      `exporter-${(Math.random().toString(36) + Date.now()).substring(
+        2,
+        7
+      )}`,
+      {
+        header: 'Archiving',
+        content:
+          'Archiving messages...\nMake sure to include the assets folder relative to the file!',
+        icon: 'history',
+        timeout: 3e3
+      }
+    );
     let css = '';
-    for (const link of Array.from(document.querySelectorAll('style[data-plugin="true"]'))) {
+    for (const link of Array.from(
+      document.querySelectorAll('style[data-powercord="true"]')
+    )) {
       css += link.innerText;
     }
     let discordcss;
-    for (const link of Array.from(document.querySelectorAll('link[rel="stylesheet"][href*="/assets/"]'))) {
+    for (const link of Array.from(
+      document.querySelectorAll(
+        'link[rel="stylesheet"][href*="/assets/"]'
+      )
+    )) {
       const r = await get(link.href);
       css += await r.body.toString();
       discordcss = link.href.split('/')[link.href.split('/').length - 1];
-      await fs.promises.writeFile(join(__dirname, 'exports', 'assets', link.href.split('/')[link.href.split('/').length - 1]), css);
+      await fs.promises.writeFile(
+        join(
+          __dirname,
+          'exports',
+          'assets',
+          link.href.split('/')[link.href.split('/').length - 1]
+        ),
+        css
+      );
     }
     const doneMap = {};
-    for (const e of [ ...messages.matchAll(/(?:(?:src|href)=")((?:https?:\/\/(?:cdn|media|(?:images-ext-\d))?\.discord(?:app?)\.(?:com|net))([^"]*))(?:")/g) ]) {
+    for (const e of [
+      ...messages.matchAll(
+        /(?:(?:src|href)=")((?:https?:\/\/(?:cdn|media|(?:images-ext-\d))?\.discord(?:app?)\.(?:com|net)|(?:\/assets\/))([^"]*))(?:")/g
+      )
+    ]) {
+      if (e[1] === '' || !e[1]) {
+        return;
+      }
       let n;
       if (!doneMap[e[2]]) {
         console.log(e);
-        n = (!(/cdn|media|(?:images-ext-\d).discordapp.(?:com|net)/g).test(e[2]) ? e[2].split('/')[e[2].split('/').length - 2] : e[2].split('/').split(5).join('.')).replace(/(\?.*)/g, '');
-        const r = await get((/https?:\/\/(?:cdn|media|(?:images-ext-\d))/g).test(e[1]) ? e[1].replace('&amp;', '&') : `${window.location.origin}${e[1]}`);
-        console.log(e[2].split('/'));
-        console.log(n);
-        const path = join(__dirname, 'exports', 'assets', 'cdn', n);
-        console.log(r);
-        await fs.promises.writeFile(path, r.body);
-        doneMap[e[2]] = n;
+        n = e[2]
+          .replace(/\//g, '-')
+          .split('?')[0]
+          .split('/')
+          .filter((eb) => eb !== '')
+          .join('-')
+          .replace('-mp4', '.mp4');
+        try {
+          const r = await get(
+            !(/\/assets\//g).test(e[1])
+              ? e[1].replace('&amp;', '&')
+              : `${window.location.origin}${e[1]}`
+          );
+          console.log(e[2].split('/'));
+          console.log('n', n);
+          const path = join(__dirname, 'exports', 'assets', 'cdn', n);
+          console.log(r);
+          await fs.promises.writeFile(path, r.body);
+          doneMap[e[2]] = n;
+        } catch (error) {
+          console.error(error);
+        }
       } else {
         n = doneMap[e[2]];
       }
-      messages = messages.replace(e[1], `./assets/cdn/${n}`);
+      if (n) {
+        messages = messages.replace(e[1], `./assets/cdn/${n}`);
+      }
     }
-    // messages.replace()
+    // button thing is a hack until i figure out how to remove them entirely
     const htmlTemplate = `
-    <html class="${document.documentElement.className.replace('mouse-mode ', '')}">
+    <html class="${`${document.documentElement.className.replace(
+    'mouse-mode ',
+    ''
+  )} mouse-mode`}">
         <head>
             <link rel="stylesheet" href="./assets/${discordcss}">
+            <style>
+            .buttonContainer-DHceWr {
+              display: none;
+            }
+            </style>
         </head>
         <body>
         <div class="scroller-2LSbBU auto-Ge5KZx scrollerBase-289Jih disableScrollAnchor-3V9UtP" dir="ltr" style="overflow: hidden scroll; padding-right: 0px;">
@@ -94,7 +157,7 @@ module.exports = class Exporter extends Plugin {
         </body>
     </html>
 
-    `;// .replace(/(?<!discord.com)(\/assets\/)/g, 'https://canary.discord.com/assets/');
+    `; // .replace(/(?<!discord.com)(\/assets\/)/g, 'https://canary.discord.com/assets/');
     const htmlPath = join(__dirname, 'exports', `${start}-${end}.html`);
     await fs.promises.writeFile(htmlPath, htmlTemplate);
     shell.openPath(htmlPath);
@@ -108,7 +171,6 @@ module.exports = class Exporter extends Plugin {
     powercord.api.commands.unregisterCommand('archive');
   }
 
-
   handleAutocomplete (args) {
     if (args.length === 0) {
       return false;
@@ -116,18 +178,24 @@ module.exports = class Exporter extends Plugin {
 
     if (args[0] === '' || args[0] === ' ') {
       return {
-        commands: [ {
-          command: 'Please input the start message ID, or leave blank to archive all loaded messages...',
-          instruction: true
-        } ]
+        commands: [
+          {
+            command:
+              'Please input the start message ID, or leave blank to archive all loaded messages...',
+            instruction: true
+          }
+        ]
       };
     }
     if (args[1] === '' || args[1] === ' ') {
       return {
-        commands: [ {
-          command: 'Input the end message ID, or leave blank to archive all loaded messages since the start...',
-          instruction: true
-        } ]
+        commands: [
+          {
+            command:
+							'Input the end message ID, or leave blank to archive all loaded messages since the start...',
+            instruction: true
+          }
+        ]
       };
     }
   }
